e2459ef5c7f08ea065ec13a948383eff8e1b3e89
BrunoQcx <brunotavm@gmail.com>
2017-08-01 23:25:41 -0300

    creating posts

--- spec/features/post_spec.rb
+++ spec/features/post_spec.rb
@@ -4,12 +4,32 @@ describe 'navigate' do
   describe 'index' do
     it 'can be reached' do
       visit posts_path
+
       expect(page.status_code).to eq(200)
     end
 
     it 'has a title of posts' do
       visit posts_path
+
       expect(page).to have_content(/Posts/)
+    end
+  end
+
+  describe 'creation' do
+    it 'has a new form that can be reached' do
+      visit new_post_path
+
+      expect(page.status_code).to eq(200)
+    end
+
+    it 'can be created from the new form page' do
+      visit new_post_path
+
+      fill_in 'post[date]', with: Date.today
+      fill_in 'post[rationale]', with: "Novo Post"
+      click_on 'Save'
+
+      expect(page).to have_content("Novo Post")
     end
   end
 end
