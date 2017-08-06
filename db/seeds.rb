e24193a93ebac09fe35f8f44e8d05cbceffc4ede
BrunoQcx <brunotavm@gmail.com>
2017-08-06 04:02:50 -0300

    index for posts

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,6 @@
+user = User.create(email: 'test@test.com', password: 'asdfasdf', password_confirmation: 'asdfasdf', first_name: 'bruno', last_name: 'tavares')
+
 100.times do |post|
-  Post.create(date: Date.today, rationale: "#{post} rationale content")
+  Post.create(date: Date.today, rationale: "#{post} rationale content", user: user)
 end
 p '100 posts created'
--- spec/features/post_spec.rb
+++ spec/features/post_spec.rb
@@ -2,6 +2,8 @@ require 'rails_helper'
 
 describe 'navigate' do
   before do
+    user = User.create(email: 'post_spec_feature@test.com', password: 'asdfasdf', password_confirmation: 'asdfasdf', first_name: 'bruno', last_name: 'tavares')
+    login_as(user, scope: :user)
     visit posts_path
   end
 
@@ -13,12 +15,17 @@ describe 'navigate' do
     it 'has a title of posts' do
       expect(page).to have_content(/Posts/)
     end
+
+    it 'has a list of posts' do
+      post1 = Post.create(date: Date.today, rationale: "blah", user: User.first)
+      post1 = Post.create(date: Date.today, rationale: "blahblah", user: User.first)
+      visit posts_path
+      expect(page).to have_content(/blah|blahblah/)
+    end
   end
 
   describe 'creation' do
     before do
-      user = User.create(email: 'post_spec_feature@test.com', password: 'asdfasdf', password_confirmation: 'asdfasdf', first_name: 'bruno', last_name: 'tavares')
-      login_as(user, scope: :user)
       visit new_post_path
     end
 
