241a8c1be9ccb715b1aaee9f9144fa0ee46f40c2
BrunoQcx <brunotavm@gmail.com>
2017-08-14 00:55:51 -0300

    implemented edit post status

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,7 @@
 user = User.create(email: 'test@test.com', password: 'asdfasdf', password_confirmation: 'asdfasdf', first_name: 'bruno', last_name: 'tavares')
 
+AdminUser.create(email: 'admin@test.com', password: 'asdfasdf', password_confirmation: 'asdfasdf', first_name: 'admin', last_name: 'tavares')
+
 100.times do |post|
   Post.create(date: Date.today, rationale: "#{post} rationale content", user: user)
 end
