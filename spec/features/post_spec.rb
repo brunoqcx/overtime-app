a5bd1cbae739d5f9ae50d78ab07e129ac719d6f3
BrunoQcx <brunotavm@gmail.com>
2017-08-15 22:39:18 -0300

    refactored some posts tests

--- spec/features/post_spec.rb
+++ spec/features/post_spec.rb
@@ -1,9 +1,19 @@
 require 'rails_helper'
 
 describe 'navigate' do
+  let(:user) { FactoryGirl.create(:user) }
+
+  let(:post) do
+    Post.create(date: Date.today, rationale: "blah", user_id: user.id)
+  end
+
+  let(:second_post) do
+    Post.create(date: Date.yesterday, rationale: "blah blah", user_id: user.id)
+  end
+
   before do
-    @user = FactoryGirl.create(:user)
-    login_as(@user, scope: :user)
+    login_as(user, scope: :user)
+    user.posts << [post, second_post]
     visit posts_path
   end
 
@@ -17,21 +27,11 @@ describe 'navigate' do
     end
 
     it 'has a list of posts' do
-      post = FactoryGirl.create(:post)
-      post.update(user_id: @user.id)
-      second_post = FactoryGirl.create(:second_post)
-      second_post.update(user_id: @user.id)
-
-      visit posts_path
-
       expect(page).to have_text(post.rationale)
       expect(page).to have_text(second_post.rationale)
     end
 
     it 'has a scope so that only creators can see their posts' do
-      post1 = Post.create(date: Date.today, rationale: "asdf", user_id: @user.id)
-      post2 = Post.create(date: Date.today, rationale: "asdf", user_id: @user.id)
-
       other_user = User.create(first_name: 'Non', last_name: 'Authorized', email: "nonauth@example.com", password: "asdfasdf", password_confirmation: "asdfasdf")
       post_from_other_user = Post.create(date: Date.today, rationale: "This post shouldn't be seen", user_id: other_user.id)
 
@@ -52,12 +52,13 @@ describe 'navigate' do
 
   describe 'delete' do
     it 'can be deleted' do
-    @post = FactoryGirl.create(:post)
-    @post.update(user_id: @user.id)
+    old_posts_count = user.posts.count
+
     visit posts_path
 
-    click_link ("delete_post_#{@post.id}_from_index")
+    click_link ("delete_post_#{second_post.id}_from_index")
     expect(page.status_code).to eq(200)
+    expect(old_posts_count - 1).to eq(user.posts.count)
     end
   end
 
@@ -88,14 +89,8 @@ describe 'navigate' do
   end
 
   describe 'edit' do
-    before do
-      @edit_user = User.create(first_name: "asdf", last_name: "asdf", email: "asdfasdf@asdf.com", password: "asdfasdf", password_confirmation: "asdfasdf")
-      login_as(@edit_user, :scope => :user)
-      @edit_post = Post.create(date: Date.today, rationale: "asdf", user_id: @edit_user.id)
-    end
-
     it 'can be edited' do
-      visit edit_post_path(@edit_post)
+      visit edit_post_path(post)
 
       fill_in 'post[date]', with: Date.tomorrow
       fill_in 'post[rationale]', with: 'edited post'
@@ -110,7 +105,7 @@ describe 'navigate' do
       non_authorized_user = FactoryGirl.create(:non_authorized_user)
       login_as(non_authorized_user, :scope => :user)
 
-      visit edit_post_path(@edit_post)
+      visit edit_post_path(post)
 
       expect(current_path).to eq(root_path)
     end
