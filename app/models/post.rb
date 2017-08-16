9d0a6313c78445179e75f7afc759b0bb0391f9f1
BrunoQcx <brunotavm@gmail.com>
2017-08-15 22:02:27 -0300

    implemented scope posts by user

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,4 +3,6 @@ class Post < ActiveRecord::Base
   validates_presence_of :date, :rationale
 
   enum status: { submitted: 0, approved: 1, rejected: 2 }
+
+  scope :posts_by, ->(user) { where(user_id: user.id) }
 end
