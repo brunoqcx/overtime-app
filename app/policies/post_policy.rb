29c5a8a672873c0408f019b3d3b5aec5ee07dcbe
BrunoQcx <brunotavm@gmail.com>
2017-08-15 23:21:11 -0300

    Restricted edition after approval only to admins

--- app/policies/post_policy.rb
+++ app/policies/post_policy.rb
@@ -1,5 +1,18 @@
 class PostPolicy < ApplicationPolicy
   def update?
-    record.user_id == user.id || admin_types.include?(user.type)
+    (user_or_admin && !post_approved?) || admin?
   end
+
+  private
+    def admin?
+      admin_types.include?(user.type)
+    end
+
+    def user_or_admin
+      record.user_id == user.id || admin?
+    end
+
+    def post_approved?
+      record.approved?
+    end
 end
