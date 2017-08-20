e0218176035facece7f9fc0a0193ba803baa094f
BrunoQcx <brunotavm@gmail.com>
2017-08-20 06:48:23 -0300

    added audit logs

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,12 +1,14 @@
 class User < ActiveRecord::Base
   has_many :posts
+  has_many :audit_logs
+  
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
   validates_presence_of :first_name, :last_name, :phone
 
   PHONE_REGEX = /\A[0-9]*\Z/
-  
+
   validates_format_of :phone, with: PHONE_REGEX
   validates :phone, length: { is: 10 }
 
